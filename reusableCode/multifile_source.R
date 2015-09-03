abb08a0ae75af8b70872deb90ae713cc04b6815d
cflagg <cflagg@neoninc.org>
2015-09-03 10:49:34 -0600

    source file for multiple file functions

---
+++ reusableCode/multifile_source.R
@@ -0,0 +1,11 @@
+# source for file merging
+
+# the file list here should point to "fileGrab1" for this specific script
+multipleCombine <- function(input, ply = llply){
+  ply(input, function(x){
+    t <- read.csv(x, header=TRUE, sep=",",stringsAsFactors = FALSE) # read the csv
+    t1 <- rbind(t) # rbind it to a temporary variable
+    return(t1) # return the full variable
+  }
+  )
+}
