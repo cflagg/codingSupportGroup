a5ae765bf4e2fdeb0584d5007ab5896fd8a3bf51
cflagg <cflagg@neoninc.org>
2016-01-21 14:30:01 -0700

    updated XLS file merging -- function combines multiple worksheets within single xls file (name = multiWorksheetCombine_xls...)

--- reusableCode/multifile_source.R
+++ reusableCode/multifile_source.R
@@ -1,4 +1,7 @@
 # source for file merging
+library(XLConnect)
+library(plyr)
+
 
 # the file list here should point to "fileGrab1" for this specific script
 multiCombine <- function(input, ply = llply){
@@ -22,4 +25,30 @@ multiCombine_xls <- function(fileList, ...){
 )
 }
 
-# function for combining multiple worksheets in a single .xls file
+setwd("C:/Users/cflagg/Documents/GitHub/codingSupportGroup/FSUTools/")
+f <- ("C:/Users/cflagg/Documents/GitHub/codingSupportGroup/FSUTools/multi_worksheet_ex.xlsx")
+
+# create a function to pull all of the worksheets together into a single data frame
+multiWorksheetCombine_xls <- function(input){
+  # loading excel workbook
+  #output <- data.frame()
+  #browser()
+  # create an empty object to fill
+  output <- NULL
+  t <- loadWorkbook(input)
+  # variable to pass sheet names
+  sheets_list <- getSheets(t)
+  # iterate through every sheet, load workbook 't' and worksheet 's'
+  for (sh in unique(sheets_list)){
+    w <- readWorksheet(t, sheet = sh, header = TRUE)
+    # fix herbaceous clip column names
+    # add the date
+    # print(head(t2))
+    # append the names from the worksheet to the data.frame
+    output <- rbind.fill(w, output)
+  }
+  return(output)
+}
+
+
+
