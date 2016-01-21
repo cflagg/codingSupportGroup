ec860d96f6d3993a11c40c2945b64c156afcd079
kjones13 <kjones@neoninc.org>
2016-01-21 15:50:06 -0700

    another way to merge tabs to one object

---
+++ FSUTools/multi_worksheet_2df.r
@@ -0,0 +1,15 @@
+library(readxl)
+library(data.table)
+
+read_excel_allsheets <- function(filename) {
+  sheets <- readxl::excel_sheets(filename)
+  x <-    lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  names(x) <- sheets
+  x
+}
+
+mysheets <- read_excel_allsheets("C:/your/filepath")
+
+#mysheets <- read_excel_allsheets("H:/Phenology/Species selection/task_1b_cohort_two_preliminary_selection_6Jan2016.xls")
+
+newDF <- rbindlist(mysheets, fill=TRUE)
