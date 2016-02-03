0d8c84b332131c1aa155e82c4333b3eef5efd627
cflagg <cflagg@neoninc.org>
2016-02-03 15:31:45 -0700

    updates to TOS schedule viewer, plotID spatial data merger, and some re-org

--- FSUTools/reporting_scripts/TOS_schedule.R
+++ FSUTools/reporting_scripts/TOS_schedule.R
@@ -9,9 +9,8 @@
 
 #############################
 
-
 # this script lays out an example for combining multiple Excel files or multiple sheets within a single Excel file
-# Cody Flagg - 6/17/2015
+# Cody Flagg - 6/17/2015; updated 2/2/2016
 
 library(XLConnect)
 library(plyr)
@@ -23,17 +22,13 @@ library(lazyeval)
 
 # source() this file in the checker
 
-# Set working directory and file path
-setwd('C:/Users/cflagg/Documents/Test/fopsCalendar')
+## input file
+xlsGrab <- "C:/Users/cflagg/Documents/GitHub/codingSupportGroup/FSUTools/reporting_scripts/TOS Protocol Implementation 2015b.xlsx"
 
-#set this to the prefix for the sheets in your module; make specific to file name batch
-prefix<-'.xlsx'
 
-# load and inspect files
-files <- list.files(getwd(), full.names=TRUE) # list all the files, full.names=TRUE is necessary for ldplay/lapply to work below
-
-xlsGrab <- files[grep(prefix,files)] # subset to just the ones in your module, using prefix
-
+#################################################################################################################################
+################################################# WRANGLING FUNCTION #############################################################
+#################################################################################################################################
 # create a function to pull all of the worksheets together into a single data frame
 scheduler <- function(input){
     # loading excel workbook
@@ -44,6 +39,9 @@ scheduler <- function(input){
     t <- loadWorkbook(input)
     # variable to pass sheet names
     sheets_list <- getSheets(t)
+    # cheap error handling: drop sheets that are probably blank; invert returns the opposite result, a negative index doesn't work
+    sheets_list <- sheets_list[grep(pattern = "Sheet|sheet", sheets_list, invert = TRUE)]
+    # drop sheet if the name contains "Sheet" e.g. "Sheet1"
     # iterate through every sheet, load workbook 't' and worksheet 's'
     for (sh in unique(sheets_list)){
       w <- readWorksheet(t, sheet = sh, header = TRUE)
@@ -59,8 +57,10 @@ scheduler <- function(input){
     return(output)
 }
 
+
 # run the function
 sched <- scheduler(xlsGrab)
+# sched <- scheduler(tosSched)
 # remove whitespace from site so that spacing is uniform
 sched$Site <- str_trim(sched$Site)
 # create a nice siteID
@@ -72,11 +72,11 @@ sched2 <- apply(sched[,c(1:16)], 2, function(x) ifelse(grepl(pattern ="[a-zA-z]"
 # rejoin with sched, leave out siteID and date
 sched <- cbind(sched[c("siteID","date")], sched2)
 
-# create a real date ('rDate')
-sched$rDate <- paste(str_sub(sched$date, 5,6),"/",str_sub(sched$date, -2,-1),"/",str_sub(sched$date,1,4),sep="")
+# create a real date ('realDate')
+sched$realDate <- paste(str_sub(sched$date, 5,6),"/",str_sub(sched$date, -2,-1),"/",str_sub(sched$date,1,4),sep="")
 
 # turn it into a date format that R recognizes, tell it what the input form is with 'format = '
-sched$rDate <- as.Date(sched$rDate,format = '%m/%d/%Y')
+sched$realDate <- as.Date(sched$realDate,format = '%m/%d/%Y')
 # solution adapted from:
 # http://stackoverflow.com/questions/27968853/making-a-presence-absence-timeline-in-r-for-multiple-y-objects
 
@@ -100,78 +100,26 @@ sched_vis <- function(data, prot, gap = 15){
     # the string variable 'prot' from above
     # source: http://stackoverflow.com/questions/26492280/non-standard-evaluation-nse-in-dplyrs-filter-pulling-data-from-mysql
     criteria <- interp(~ prot == TRUE, prot = as.name(prot))
-    data <- data %>% select_("rDate", "siteID", prot)
+    data <- data %>% select_("realDate", "siteID", prot)
     
     # arrange dates into ascending order, so the lag is calculated correctly  --- MUST DO THIS ---
-    data <- arrange_(data, "siteID", "rDate")
+    data <- arrange_(data, "siteID", "realDate")
     
     # THIS WORKS - consecutive = less than or equal to a 14 day gap between sampling dates
-    data <- data %>% select_("siteID", "rDate", prot) %>% group_by_("siteID", prot) %>% mutate(consecutive=c(diff(rDate),gap)<=(gap-1))
+    data <- data %>% select_("siteID", "realDate", prot) %>% group_by_("siteID", prot) %>% mutate(consecutive=c(diff(realDate),gap)<=(gap-1))
     
     # THIS WORKS -- filter() limits the graph to just sites that have had the sampling, else the plot becomes cluttered
     # filter_ needs the statement to be crafted as per above for object 'criteria'
-    data %>% filter_(criteria) %>% ggplot(aes(rDate, siteID)) + 
+    data %>% filter_(criteria) %>% ggplot(aes(realDate, siteID)) + 
       geom_line(aes(alpha=consecutive, group=siteID),size=2,color="blue")+ geom_point(aes(group=siteID),size=3.5, color = c("red"))  + theme_bw() +
       scale_alpha_manual(values=c(0, 1), breaks=c(FALSE, TRUE), guide='none') + ggtitle(prot)
 }
 
+
+#################################################################################################################################
+#################################################################################################################################
 sched_vis(sched, "Veg..Characterization")
 names(sched)
 sched_vis(sched, "Coarse.Downed.Wood")
-#################################################################################################################################
-#################################################################################################################################
 
 
-#################################################### VESTIGIAL ####################################################
-
-
-## Min/Max VC sampling dates for KJ ## 
-# only include dates where sampling occurred
-# sched4b %>% filter(Veg..Characterization == TRUE) %>% select(-consecutive) -> vc_dates
-# 
-# # calculate first and last dates of sampling by site
-# vc_dates_sum <- ddply(vc_dates, .(siteID), summarize, firstDate = min(rDate), lastDate = max(rDate))
-# 
-# # siteID/domainID hash table
-# siteDomain <- read.csv("N:/Science/FSU/FOPSDataEntry/2015/plotID_siteID_tables/plotIDs_2015_09052015.csv")
-# 
-# siteDomain_hash <- siteDomain %>% select(domainID, siteID, stateProvince)
-# 
-# siteDomain_hash <- unique(siteDomain_hash)
-# 
-# sd_out <- left_join(vc_dates_sum, siteDomain_hash, by = "siteID")
-# 
-# write.csv(sd_out, file = "C:/Users/cflagg/Documents/Test/vc_dates.csv")
-# 
-# vc_dates_out <- arrange(vc_dates)
-# 
-# write.csv(vc_dates_out, file = "C:/Users/cflagg/Documents/Test/vc_dates_all.csv")
-# 
-# write.csv(sched, file = "C:/Users/cflagg/Documents/Test/TOS_implementation_compiled.csv")
-
-# sched4c <- sched4 %>% select(siteID,rDate,Veg.Structure)
-
-
-# # what happens when they are ordered correctly? --- THIS WORKS --- this is less than or equal to am 8 day sampling gap
-# sched4 %>% group_by(siteID,Veg.Structure)  %>% mutate(consecutive=c(diff(rDate),9)<=8) %>% 
-#   filter(siteID == "JERC",Veg.Structure==TRUE) %>% ggplot(aes(rDate,siteID)) + geom_point(aes(group=siteID),size=3) + 
-#   geom_line(aes(alpha=consecutive,group=siteID))
-# 
-# # just points, no lines
-# sched4 %>% filter(Veg.Structure == TRUE) %>% ggplot(aes(rDate, siteID)) + 
-#   geom_point(aes(group=siteID),size=2, color = c("red")) + theme_bw() 
-# 
-# # how to understand consecutive dating of sampling dates, for visualization purposes:
-# # if the lagged difference in dates is less than or equal to a value, then give it the value TRUE
-# 
-# # group_by, mutate, diff, consecutive test
-# m  <- data.frame(a = c(1,3,5,7,12,14), b = c(rep("a",3),rep("b",3))) # supply the data
-# # need to concatenate, c(), with diff() because it returns a vector of n-1, thus adding another value to complete the vector
-# m %>% group_by(b) %>% mutate(consecutive=c(diff(a),1)<=2)
-# 
-# # other potentially useful bind functions
-# # rbind.fill
-# 
-# # rbindList
-# 
-# # smartbind
